5ed160ea4eec1c8c8e31912edcaf42060d1997ee
sttronn <rishav.21m@gmail.com>
2021-02-09 22:32:36 +0530

    added debouncing

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -5,6 +5,25 @@ export const format = (number) => {
 export const percent = (total, delta) =>
   parseFloat((delta / total) * 100).toFixed(2);
 
-export const getValueFromKey = (list, key) => {
-  return Object.values(list.filter((e) => Object.keys(e)[0] === key)[0])[0];
+export const debounce = (func, wait, immediate) => {
+  var timeout;
+
+  return function () {
+    var context = this,
+      args = arguments;
+
+    var callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function () {
+      timeout = null;
+
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    }, wait);
+
+    if (callNow) func.apply(context, args);
+  };
 };
